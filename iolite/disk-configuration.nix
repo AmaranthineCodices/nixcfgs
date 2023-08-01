2aacd7854ebb0a10e6b91e61e4498c9e3c3f382c
Lily Brown <git@lily.fyi>
2023-07-31 17:43:12 -0700

    testing

--- iolite/disk-configuration.nix
+++ iolite/disk-configuration.nix
@@ -1,23 +1,25 @@
-{ disks ? [ "/dev/vda" ], ... }:
+{ ... }:
 {
-  disk = {
-    main = {
-      type = "disk";
-      device = builtins.elemAt disks 0;
-      content = {
-        type = "table";
-        format = "gpt";
-        partitions = [
-          {
-            name = "root";
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
-            };
-          }
-        ];
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "root";
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            }
+          ];
+        };
       };
     };
   };
